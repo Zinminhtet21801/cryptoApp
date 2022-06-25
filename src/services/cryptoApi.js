648ce500c2ea6fdc36b2532ca4b62bbddf9d4df2
Zin Min Htet <zetminhtin@gmail.com>
2022-06-26 01:03:48 +0630

    change api key

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
 const cryptoApiHeaders = {
-  'x-access-token': process.env.REACT_APP_COIN_RANKING_TOKEN,
+  'x-access-token': process.env.REACT_APP_COIN_RANKING_TOKEN.split("coinranking")[1],
   'no-cors': true,
 };
 
